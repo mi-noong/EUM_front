3637aaef960e887b227db0d6601e581041ce5658
soeun <ssoarmy4537@gmail.com>
2025-07-05 12:42:10 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart'; //카카오 SDK 초기화(소셜로그인 연동에 필요)
 import 'SplashScreen.dart';
 
 void main() {
+  KakaoSdk.init(nativeAppKey: 'c15bd6ab806014b0fde0e7a2f5b4de7e');
   runApp(MyApp());
 }
 
